21b373d1019ed667381b3be77140f9a929fbdf3a
ConorOBrien-Foxx <conorobrien4god@gmail.com>
2020-06-16 18:28:05 -0400

    add console command for windows

---
+++ fqr.cmd
@@ -0,0 +1,8 @@
+REM taken from fx
+@IF EXIST "%~dp0\node.exe" (
+    "%~dp0\node.exe"  "%~dp0\node_modules\fqr\index.js" %*
+) ELSE (
+    @SETLOCAL
+    @SET PATHEXT=%PATHEXT:;.JS;=;%
+    node  "%~dp0\node_modules\fqr\index.js" %*
+)
